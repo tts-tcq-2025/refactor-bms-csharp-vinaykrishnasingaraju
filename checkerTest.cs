3524b900a34da66271e0306a81062cf16a369a64
Vinay Krishna <76031406+vinaykrishnasingaraju@users.noreply.github.com>
2025-08-17 09:46:35 +0530

    Update checkerTest.cs

--- checkerTest.cs
+++ checkerTest.cs
@@ -1,11 +1,57 @@
-using Xunit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-public class CheckerTests
+namespace VitalCheckerTests
 {
-    [Fact]
-    public void NotOkWhenAnyVitalIsOffRange()
+    [TestClass]
+    public class CheckerTests
     {
-        Assert.False(Checker.VitalsOk(99f, 102, 70));
-        Assert.True(Checker.VitalsOk(98.1f, 70, 98));
+        [TestMethod]
+        public void NotOkWhenTemperatureOutOfRange()
+        {
+            Assert.IsFalse(Checker.VitalsOk(104f, 70, 98));
+        }
+
+        [TestMethod]
+        public void NotOkWhenPulseOutOfRange()
+        {
+            Assert.IsFalse(Checker.VitalsOk(98.6f, 120, 98));
+        }
+
+        [TestMethod]
+        public void NotOkWhenSpo2OutOfRange()
+        {
+            Assert.IsFalse(Checker.VitalsOk(98.6f, 70, 85));
+        }
+
+        [TestMethod]
+        public void OkWhenAllVitalsInRange()
+        {
+            Assert.IsTrue(Checker.VitalsOk(98.6f, 70, 98));
+        }
+
+        [TestMethod]
+        public void TemperatureEdgeCases()
+        {
+            Assert.IsTrue(Checker.VitalsOk(95f, 70, 98));
+            Assert.IsTrue(Checker.VitalsOk(102f, 70, 98));
+            Assert.IsFalse(Checker.VitalsOk(94.9f, 70, 98));
+            Assert.IsFalse(Checker.VitalsOk(102.1f, 70, 98));
+        }
+
+        [TestMethod]
+        public void PulseEdgeCases()
+        {
+            Assert.IsTrue(Checker.VitalsOk(98.6f, 60, 98));
+            Assert.IsTrue(Checker.VitalsOk(98.6f, 100, 98));
+            Assert.IsFalse(Checker.VitalsOk(98.6f, 59, 98));
+            Assert.IsFalse(Checker.VitalsOk(98.6f, 101, 98));
+        }
+
+        [TestMethod]
+        public void Spo2EdgeCases()
+        {
+            Assert.IsTrue(Checker.VitalsOk(98.6f, 70, 90));
+            Assert.IsFalse(Checker.VitalsOk(98.6f, 70, 89));
+        }
     }
 }
